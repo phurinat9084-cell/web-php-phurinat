d0609ee4913aa394ca5e95d06e6ff66ac2e834bd
phurinat9084-cell <phurinat9084@gmail.com>
2026-06-08 16:36:51 +0700

    assignment week3

---
+++ week3.php
@@ -0,0 +1,10 @@
+<?php
+$name = "ภูรินาท สำลีแก้ว";
+$age  = 18;
+$department  = "เทคโนโลยีสารสนเทศ";
+$id = "69319010017";
+echo "ชื่อ: $name <br>";
+echo "อายุ: $age <br>";
+echo "แผนก: $department <br>";
+echo "รหัสประจำตัว: $id <br>";
+?>
